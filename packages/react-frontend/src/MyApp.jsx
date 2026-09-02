e8909a771729b00df539fc6c2a41898c281e9c81
celeste-art09 <cearteag@calpoly.edu>
2026-09-02 11:18:52 -0700

    IE1-Step5Done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
-const characters = [
+
+function MyApp() {
+    const [characters, setCharacters] = useState([
     {
         name: "Charlie",
         job: "Janitor"
@@ -17,15 +19,19 @@ const characters = [
     {
         name: "Dennis",
         job: "Bartender"
+    }   
+    ]);
+    function removeOneCharacter(index){
+        const updated = characters.filter((character,i) => {
+            return i !== index;
+        });
+        setCharacters(updated);
     }
-];
-
-function MyApp() {
-  return (
-    <div className="container">
-      <Table characterData={characters} />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        </div>
+    );
 }
 
 export default MyApp;
