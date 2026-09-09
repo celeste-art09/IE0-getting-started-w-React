b4623c53b9b20527458e60c9a46c910595c38167
celeste-art09 <cearteag@calpoly.edu>
2026-09-09 16:40:23 -0700

    IE2-Step3Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,5 +41,5 @@ app.get("/users", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}/users`);
 });
