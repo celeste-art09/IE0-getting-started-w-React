40b6fe60ac787f7d767c182125a38827bf16f4f1
celeste-art09 <cearteag@calpoly.edu>
2026-09-11 14:38:58 -0700

    IE2-Step5Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,6 +4,11 @@ import express from "express";
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 }
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+
 const users = {
     users_list :[
         {
@@ -49,6 +54,16 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
   });
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined){
+    res.status(404).send("Resource not found");
+  } else {
+    res.send(result);
+  }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
